7a6a00eeef5fb7d4cf94021cbc6adb9891b590a7
Caleb Hostetler <18chostetler@gmail.com>
2025-05-04 21:53:18 -0400

    Create basic structure

--- matches/admin.py
+++ matches/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Region, Team, Room, Matchup, MatchupSource
 
 # Register your models here.
+
+admin.site.register(Region)
+admin.site.register(Team)
+
--- matches/models.py
+++ matches/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+class Region(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    color = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Team(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class Room(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Matchup(models.Model):
+    match_number = models.IntegerField(unique=True, validators=[MinValueValidator(100)])
+    start_time = models.DateTimeField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.match_number)
+
+class MatchupSource(models.Model):
+    matchup = models.ForeignKey(Matchup, on_delete=models.CASCADE)
+
+
+
+    
