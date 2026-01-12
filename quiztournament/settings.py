4e5f3ec8942af42f0a0e7a900b1312ecbd5fcd36
Caleb Hostetler <18chostetler@gmail.com>
2026-01-11 19:38:03 -0500

    Update to use pythonanywhere

--- quiztournament/settings.py
+++ quiztournament/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+__d+okgx4zij@-(rz^umogr1g6y^edl77#725!_d_#!&3!&43'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chostetler.pythonanywhere.com']
 
 
 # Application definition
